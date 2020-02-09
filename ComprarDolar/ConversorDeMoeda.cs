4fec339e1b4f6b9fd1032c758d0033a3d1f84df2
Vinicius Maurente <viniciusvmaurente@gmail.com>
2020-02-09 12:33:57 -0300

    Projeto em C# iniciado, para calcular a compra de dolar considerando com custo de IOF

---
+++ ComprarDolar/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace ComprarDolar {
+    class ConversorDeMoeda {
+
+        public static double Iof = 6.38;
+
+        public static double DolarParaReal(double quantia, double cotacao) {
+            double total = quantia * cotacao;
+            return total + total * Iof / 100.0;
+        
+        }
+    }
+}
---
+++ ComprarDolar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ComprarDolar {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Cotacao do dolar: US$ ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+            Console.Write("Valor a ser pago em REAIS com IOF = R$ " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
